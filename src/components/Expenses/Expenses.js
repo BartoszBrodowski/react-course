93aae76a9987a38531ff3145c4a88c4329a95df6
Bartosz Brodowski <bartolini10058@gmail.com>
2022-06-05 22:59:44 +0200

    Form element created

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,43 @@
+import Card from '../ui/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import { useState } from 'react';
+
+const Expenses = ({ expenses }) => {
+	const [filteredYear, setFilteredYear] = useState('2020');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	return (
+		<div>
+			<Card className='expenses'>
+				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+				<ExpenseItem
+					title={expenses[0].title}
+					amount={expenses[0].amount}
+					date={expenses[0].date}
+				/>
+				<ExpenseItem
+					title={expenses[1].title}
+					amount={expenses[1].amount}
+					date={expenses[1].date}
+				/>
+				<ExpenseItem
+					title={expenses[2].title}
+					amount={expenses[2].amount}
+					date={expenses[2].date}
+				/>
+				<ExpenseItem
+					title={expenses[3].title}
+					amount={expenses[3].amount}
+					date={expenses[3].date}
+				/>
+			</Card>
+		</div>
+	);
+};
+
+export default Expenses;
